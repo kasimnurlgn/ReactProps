fac483c2fde2fb76cb147d16651b6b66facbfb4c
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-04-16 16:48:16 +0300

    some changes

--- src/components/Props/Phase1.jsx
+++ src/components/Props/Phase1.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import img1 from "../../assets/icon-phase-1.png";
+import { FaAngleRight } from "react-icons/fa";
 import "./Phase1.css";
 const Phase1 = () => {
   return (
-    <div className="container border my-5 mx-auto rounded">
+    <div className="container border my-5 mx-auto rounded phase">
       <div className="row d-flex justify-content-between align-items-center my-3">
         <div className="col-12 col-md-4 border-end text-center">
           <img
@@ -29,6 +30,9 @@ const Phase1 = () => {
               learn about VS Code editor and Chrome's Developer Tools.
             </p>
           </div>
+        </div>
+        <div className="col-12 col-md-1 d-flex align-items-center justify-content-center">
+          <FaAngleRight style={{ fontSize: "24px", color: "#000" }} />
         </div>
       </div>
     </div>
