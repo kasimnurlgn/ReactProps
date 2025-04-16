11688cf0518a971263cea57e3ce2d7ef42d124b1
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-04-16 22:22:29 +0300

    building done, using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 import "./App.css";
+import img1 from "./assets/icon-phase-1.png";
+import img2 from "./assets/icon-phase-2.png";
+import img3 from "./assets/icon-phase-3.png";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Phase1 from "./components/Phase1/Phase1";
-import Phase2 from "./components/Phase2/Phase2";
-import Phase3 from "./components/Phase3/Phase3";
+import PhaseCard from "./components/PhaseCard/PhaseCard";
+
 function App() {
   return (
     <>
-      <Phase1 />
-      <Phase2 />
-      <Phase3 />
+      <PhaseCard
+        image={img1}
+        title="Phase 1: Building static websites using HTML, CSS & Bootstrap"
+        description="In this phase, you'll learn about the underlying structure of the
+              web - HTML. You'll also learn how to apply styling to a website
+              through CSS. To help you build websites faster and easier, you
+              will learn the Bootstrap framework. Along the way, you'll also
+              learn about VS Code editor and Chrome's Developer Tools."
+      />
+      <PhaseCard
+        image={img2}
+        title="Phase 2: Learn coding with JavaScript"
+        description="JavaScript is among the most powerful and flexible programming
+              languages of the web. It powers the dynamic behavior on most
+              websites. In this phase of your course, you will learn programming
+              fundamentals using the latest JavaScript syntax. The concepts
+              covered in these lessons lay the foundation for using JavaScript
+              in any environment."
+      />
+      <PhaseCard
+        image={img3}
+        title="Phase 3: Node, Express, MySql and React.js"
+        description="React is a JavaScript library, developed in 2013 by Facebook. It
+              is very popular and used on major sites including on Facebook,
+              Netflix, Khan Academy and many others. In this phase, we will
+              start from the fundamentals and go all the way up to building full
+              applications with custom styling. Along the way, you'll also learn
+              about npm, ES6, Node.js, Express and MySql."
+      />
     </>
   );
 }
---
+++ src/components/PhaseCard/PhaseCard.jsx
@@ -0,0 +1,31 @@
+// Phase.jsx
+import React from "react";
+import { FaAngleRight } from "react-icons/fa";
+import "./PhaseCard.css";
+
+const PhaseCard = ({ image, title, description }) => {
+  return (
+    <div className="container border my-5 mx-auto rounded phase">
+      <div className="row d-flex justify-content-between align-items-center my-3">
+        <div className="col-12 col-md-4 border-end text-center">
+          <img src={image} alt="" className="img-fluid pe-0" />
+        </div>
+
+        <div className="col-12 col-md-7">
+          <div className="heading">
+            <h1 className="fs-4 fw-bolder">{title}</h1>
+          </div>
+
+          <div className="description">
+            <p>{description}</p>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-1 d-flex align-items-center justify-content-center">
+          <FaAngleRight size={30} className="angle-right" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PhaseCard;
